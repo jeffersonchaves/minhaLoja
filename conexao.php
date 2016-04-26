0ec96bd17e31a534933ce17515c51550997e1ec1
jeffersonchaves <jeffersong.s@hotmail.com>
2016-04-26 10:22:14 -0300

    Atualizando Exemplos

--- conexao.php
+++ conexao.php
@@ -12,9 +12,16 @@
 		$consulta = $conexao->query('SELECT * FROM tb_produtos');
 
 		// Busque o conteudo da consulta com o comando FETCH
-		$produto = $consulta->fetch(PDO::FETCH_ASSOC);
+		$produtos = $consulta->fetchAll(PDO::FETCH_ASSOC);
 
 		// Teste sua consulta com print_r
+		foreach ($produtos as $produto_unico) {
+
+			echo "Compre ". $produto_unico['nome_produto']."<br />";
+			echo "Valor R$".$produto_unico['preco']."<br />";
+			echo "<br/>";
+		}
+
 
 	} catch(PDOException $erro){
 
---
+++ visualizacao/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Minha Loja</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<div id="loja">
+
+	<div class="bloco">
+		<div class="produto">
+			  
+			  <div class="imagem">
+		        <img src="img/produto.jpg" alt="" class="img-responsiva">
+		      </div>
+		    
+		      <div class="texto">
+		        <h3> <a href="#">NOME</a></h3>
+		        <p class="preco">PRECO</p>
+		      </div>
+	    
+	    </div>
+	</div>
+
+</div>
+	
+</body>
+</html>
